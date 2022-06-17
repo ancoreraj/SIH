294c325e8a5186c02d10454c863a19fc2c840678
Ankit Gautam <a53gau@gmail.com>
2022-06-17 11:11:16 +0530

    minor changes in navbar

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,12 @@
+import Stack from '@mui/material/Stack';
+import { Box } from '@mui/system';
+
 const Login = () => {
     return (
-        <div>Login</div>
+        <Stack sx={{ maxWidth: "1300px", margin: '1rem auto'}} direction={"row"} justifyContent={"space-between"} >
+            <Box sx={{ flex: 1 }}>Image Box</Box>
+            <Box sx={{ flex: 1 }}>Login Form</Box>
+        </Stack>
     )
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,12 +6,14 @@ import { Box } from '@mui/system';
 import { Notifications } from '@mui/icons-material';
 import { useState } from 'react';
 
-const StyledToolbar = styled(Toolbar)({
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: "center",
-  backgroundColor: '#673ab7',
-});
+  [theme.breakpoints.up('sm')]: {
+    margin: "0 3rem"
+  }
+}));
 
 const Icons = styled(Box)(({ theme }) => ({
   gap: "20px",
@@ -23,58 +25,64 @@ const Icons = styled(Box)(({ theme }) => ({
 const UserBox = styled(Box)(({ theme }) => ({
   display: "flex",
   gap: "10px",
+  justifyContent: 'center',
   alignItems: "center",
 }))
 
 const Navbar = () => {
 
   const [menu, setMenu] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+
 
   return (
-    <AppBar position='fixed'>
+    <AppBar position='sticky' color='default'>
       <StyledToolbar>
-        <Typography variant='h6'>SiteName</Typography>
-        <Icons>
-          <Badge badgeContent={2} color='error'>
-            <Notifications />
-          </Badge>
-
-          {/* <ButtonBox> */}
-          <Button size='small' variant='outlined' color='info'>Sign Up</Button>
-          {/* </ButtonBox> */}
-          <UserBox>
-            <Avatar
-              src='https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg'
-              sx={{ width: 35, height: 35 }}
-              onClick={e => setMenu(true)}
-            />
-
-            <Menu
-              id='positioned-menu'
-              aria-labelledby='positioned-menu'
-              open={menu}
-              onClose={e => setMenu(false)}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right"
-              }}
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right"
-              }}
-            >
-              <MenuItem><AccountCircleRoundedIcon fontSize="small" sx={{ mr: 2 }} />Profile</MenuItem>
-              <MenuItem><SettingsRoundedIcon fontSize="small" sx={{ mr: 2 }} />Settings</MenuItem>
-              <Divider />
-              <MenuItem><LogoutRoundedIcon fontSize="small" sx={{ mr: 2 }} />Logout</MenuItem>
-
-            </Menu>
-            <Typography variant='subtitle1' sx={{ display: { xs: "none", sm: "block" } }}>John</Typography>
-          </UserBox>
-
-        </Icons>
-      </StyledToolbar>
-    </AppBar>
+        <Typography variant='h5'>SiteName</Typography>
+        {!isLogin ? (
+          <Button
+            variant='contained'
+            onClick={e => setIsLogin(!isLogin)}
+          >
+            Sign Up
+          </Button>
+        )
+          : (
+            < Icons >
+              <Badge badgeContent={2} color='error'>
+                <Notifications />
+              </Badge>
+              <UserBox>
+                <Avatar
+                  src='https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg'
+                  sx={{ width: 35, height: 35 }}
+                  onClick={e => setMenu(true)}
+                />
+                <Menu
+                  id='positioned-menu'
+                  aria-labelledby='positioned-menu'
+                  open={menu}
+                  onClose={e => setMenu(false)}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                  }}
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                  }}
+                >
+                  <MenuItem><AccountCircleRoundedIcon fontSize="small" sx={{ mr: 2 }} />Profile</MenuItem>
+                  <MenuItem><SettingsRoundedIcon fontSize="small" sx={{ mr: 2 }} />Settings</MenuItem>
+                  <Divider />
+                  <MenuItem><LogoutRoundedIcon fontSize="small" sx={{ mr: 2 }} />Logout</MenuItem>
+                </Menu>
+              </UserBox>
+            </Icons>
+          )
+        }
+      </StyledToolbar >
+    </AppBar >
   )
 }
 
