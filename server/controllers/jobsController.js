34a5731e5d6a5912431ab03e40889bce436a10a7
Ankur Raj <64456355+ancoreraj@users.noreply.github.com>
2022-07-20 11:09:45 +0530

    updated createJobController

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -2,25 +2,26 @@ const JobsModel = require("../models/JobsModel");
 
 const createJobController = async (req, res) => {
     try {
-        const { title, qualification, lastDate, applyLink, maxAge } = req.body;
+        const { title, qualification, lastDate, applyLink, maxAgeToApply } = req.body;
 
-        if (!title || !qualification || !lastDate || !applyLink || !maxAge) {
+        if (!title || !qualification || !lastDate || !applyLink || !maxAgeToApply) {
             return res.status(422).json({ error: "please add all the fields" });
         }
 
         const savedUser = req.user;
 
         if (savedUser.isAdmin === false) {
-            return res.status(403).json({ error: "registered user don't have access to post jobs" })
+            return res.status(403).json({ error: "You don't have access to post jobs" })
         }
         
         const newJob = new JobsModel({
             title,
             qualification,
+            organizationName: savedUser.organizationName,
             lastDate,
             applyLink,
             postedBy: savedUser,
-            maxAge
+            maxAgeToApply
         })
 
         await newJob.save((err) => {
